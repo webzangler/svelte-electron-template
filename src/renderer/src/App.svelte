04fb4b83557fa848f4e8a4062b5cf7e7d4427c56
Gurbir Dhaliwal <72323320+webzangler@users.noreply.github.com>
2024-05-05 04:52:38 +0530

    Initial Commit

---
+++ src/renderer/src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+</script>
+
+<main class="min-h-screen w-full flex items-center justify-center flex-col gap-4">
+  <h1 class="font-bold text-4xl">Electron + Svelte Starter</h1>
+  <button class="bg-blue-500 px-4 py-2 rounded-md text-white" on:click={ipcHandle}>Ping</button>
+</main>
